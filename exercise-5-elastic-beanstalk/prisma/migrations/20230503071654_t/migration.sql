fc3df31b1eb0accbcc49a8f6dab0388b168b7904
brian <htnb831maimai@gmail.com>
2024-10-13 19:56:38 +0700

    init code

--- exercise-5-elastic-beanstalk/prisma/migrations/20230503071654_t/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Tweet" (
-    "id" TEXT NOT NULL,
-    "author" TEXT NOT NULL,
-    "text" TEXT NOT NULL,
-    "imgUrl" TEXT,
-
-    CONSTRAINT "Tweet_pkey" PRIMARY KEY ("id")
-);
