c2b10073ddf81fdd33e99ac06b0517fb4c50d611
root <root@localhost>
2024-10-13 13:41:04 +0000

    add code for filteredimage endpoint

--- project starter code/server.js
+++ project starter code/server.js
@@ -1,45 +1,57 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
+import axios from 'axios';
+import imageType from 'image-type';
+import { filterImageFromURL, deleteLocalFiles } from './util/util.js';
 
+// Init the Express application
+const app = express();
 
+// Set the network port
+const port = process.env.PORT || 8082;
 
-  // Init the Express application
-  const app = express();
+// Use the body parser middleware for post requests
+app.use(bodyParser.json());
 
-  // Set the network port
-  const port = process.env.PORT || 8082;
-  
-  // Use the body parser middleware for post requests
-  app.use(bodyParser.json());
+// @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+// GET /filteredimage?image_url={{URL}}
+// endpoint to filter an image from a public url.
+// IT SHOULD
+//    1. validate the image_url query
+//    2. call filterImageFromURL(image_url) to filter the image
+//    3. send the resulting file in the response
+//    4. deletes any files on the server on finish of the response
+// QUERY PARAMETERS
+//    image_url: URL of a publicly accessible image
+// RETURNS
+//   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+app.get('/filteredimage', async (req, res) => {
+  const { image_url } = req.query;
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  if (!image_url) {
+    return res.status(400).send('image_url is required.');
+  }
 
-    /**************************************************************************** */
+  try {
+    const imageResponse = await axios.get(image_url, { responseType: 'arraybuffer' });
+    const imageBuffer = Buffer.from(imageResponse.data);
+    const { ext, mime } = imageType(imageBuffer) || { ext: 'jpg', mime: 'image/jpeg' };
+    res.set('Content-Type', mime);
+    res.status(200).send(imageBuffer);
+  } catch (error) {
+    res.status(500).send(`An error occurred: ${error.message}`);
+  }
+});
+//! END @TODO1
 
-  //! END @TODO1
-  
-  // Root Endpoint
-  // Displays a simple message to the user
-  app.get( "/", async (req, res) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+// Root Endpoint
+// Displays a simple message to the user
+app.get("/", async (req, res) => {
+  res.send("try GET /filteredimage?image_url={{}}");
+});
 
-  // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+// Start the Server
+app.listen(port, () => {
+  console.log(`server running http://localhost:${port}`);
+  console.log(`press CTRL+C to stop server`);
+});
